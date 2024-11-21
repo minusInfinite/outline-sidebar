490d4ce90619034108b7b6327dc76aeec21b5a4d
minusInfinite <ashley@minusinfinite.net>
2024-11-21 15:06:47 +1030

    cache refresh

--- assets/outline-sidebar.js
+++
@@ -1,7 +0,0 @@
-{
-    document.querySelectorAll("[data-osb-pos]").forEach((el) => {
-        el.addEventListener("click", async (e) => {
-            await syscall("editor.moveCursor", el.dataset.osbpost)
-        })
-    })
-}
